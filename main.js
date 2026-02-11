f38285e456d922f64c3a8c88ae2eb046c85199df
AlanBP18 <142277977+AlanBP18@users.noreply.github.com>
2026-02-11 15:15:52 -0500

    fix (main.js): mira si es par o impar

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function NumeroKiwis(num1){
+    return num1 * 0
+}
+
+const NumeroConejos =(num1) => {
+    const result = num1 * 1000
+    return result
+}
+
+const num1 = 20
+function par(num1){
+    if (num1 % 2 ===0){
+        return console.log("par");
+    }
+    return console.log("no par")
+}
+
+const esPar = (n) => {
+    let pink=n % 2 === 0;
+    if (pink==true){
+        return console.log("si es")
+    } return console.log("no es")
+}
